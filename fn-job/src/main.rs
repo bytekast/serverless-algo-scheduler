a49a8114384cf7ce951f2946f5ee5cef57e38d6c
Rowell Belen <developer@bytekast.com>
2019-05-29 02:00:40 -0600

    Add Scheduler

--- fn-job/src/main.rs
+++ fn-job/src/main.rs
@@ -4,7 +4,6 @@ extern crate reqwest;
 use lambda_runtime::{error::HandlerError, lambda, Context};
 use serde_json::Value;
 
-use aws_lambda_events::event::sqs::SqsEvent;
 use models::Schedule;
 use rusoto_core::Region;
 use rusoto_sqs::{SendMessageRequest, Sqs, SqsClient};
@@ -18,7 +17,6 @@ fn router(event: Value, context: Context) -> Result<Value, HandlerError> {
     let function = env::var("FUNCTION").expect("Unknown function");
     match function.as_ref() {
         "scheduler" => scheduler(event, context),
-        "runner" => runner(event, context),
         _ => Ok(Value::Null), // do nothing
     }
 }
@@ -26,6 +24,7 @@ fn router(event: Value, context: Context) -> Result<Value, HandlerError> {
 fn scheduler(event: Value, _: Context) -> Result<Value, HandlerError> {
     println!("{:?}", event);
     let schedules_to_run = Schedule::get_enabled_schedules();
+    println!("Schedules to Run: {}", schedules_to_run.len());
     for schedule in schedules_to_run {
         match serde_json::to_string(&schedule) {
             Ok(json) => {
@@ -34,45 +33,6 @@ fn scheduler(event: Value, _: Context) -> Result<Value, HandlerError> {
                 send_sqs(job_sqs_url.to_owned(), json)
             }
             Err(error) => println!("{}", error.to_string()),
-        }
-    }
-
-    Ok(Value::Null)
-}
-
-fn runner(event: Value, _: Context) -> Result<Value, HandlerError> {
-    let sqs_event: SqsEvent = serde_json::from_str(event.to_string().as_ref()).unwrap();
-    println!("{:?}", sqs_event);
-    let result_sqs_url =
-        env::var("RESULT_QUEUE_URL").expect("env variable RESULT_QUEUE_URL not found");
-    let client = reqwest::Client::builder()
-        .use_rustls_tls()
-        .build()
-        .expect("Unable to create client");
-    for record in sqs_event.records {
-        match record.body {
-            Some(body) => {
-                let schedule: Schedule = serde_json::from_str(body.as_ref()).unwrap();
-                let url = format!(
-                    "https://api.test.algorithmia.com/v1/algo/{}",
-                    schedule.algorithm
-                );
-                let res = client
-                    .post(&url)
-                    .header("Content-Type", "text/plain")
-                    .header("Authorization", schedule.apikey)
-                    .body(schedule.input)
-                    .send();
-                match res {
-                    Ok(mut res) => {
-                        let respose_text = res.text().ok().unwrap();
-                        println!("{:?}", respose_text);
-                        send_sqs(result_sqs_url.to_owned(), respose_text)
-                    }
-                    Err(e) => println!("{:?}", e),
-                }
-            }
-            None => (),
         }
     }
 
