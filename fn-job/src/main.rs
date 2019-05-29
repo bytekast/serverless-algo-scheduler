cc98dc5e577003d6daafb3f1168ef32b38744745
Rowell Belen <developer@bytekast.com>
2019-05-29 00:43:46 -0600

    Initial commit

---
+++ fn-job/src/main.rs
@@ -0,0 +1,90 @@
+extern crate models;
+extern crate reqwest;
+
+use lambda_runtime::{error::HandlerError, lambda, Context};
+use serde_json::Value;
+
+use aws_lambda_events::event::sqs::SqsEvent;
+use models::Schedule;
+use rusoto_core::Region;
+use rusoto_sqs::{SendMessageRequest, Sqs, SqsClient};
+use std::env;
+
+fn main() {
+    lambda!(router);
+}
+
+fn router(event: Value, context: Context) -> Result<Value, HandlerError> {
+    let function = env::var("FUNCTION").expect("Unknown function");
+    match function.as_ref() {
+        "scheduler" => scheduler(event, context),
+        "runner" => runner(event, context),
+        _ => Ok(Value::Null), // do nothing
+    }
+}
+
+fn scheduler(event: Value, _: Context) -> Result<Value, HandlerError> {
+    println!("{:?}", event);
+    let schedules_to_run = Schedule::get_enabled_schedules();
+    for schedule in schedules_to_run {
+        match serde_json::to_string(&schedule) {
+            Ok(json) => {
+                let job_sqs_url =
+                    env::var("JOB_QUEUE_URL").expect("env variable JOB_QUEUE_URL not found");
+                send_sqs(job_sqs_url.to_owned(), json)
+            }
+            Err(error) => println!("{}", error.to_string()),
+        }
+    }
+
+    Ok(Value::Null)
+}
+
+fn runner(event: Value, _: Context) -> Result<Value, HandlerError> {
+    let sqs_event: SqsEvent = serde_json::from_str(event.to_string().as_ref()).unwrap();
+    println!("{:?}", sqs_event);
+    let result_sqs_url =
+        env::var("RESULT_QUEUE_URL").expect("env variable RESULT_QUEUE_URL not found");
+    let client = reqwest::Client::builder()
+        .use_rustls_tls()
+        .build()
+        .expect("Unable to create client");
+    for record in sqs_event.records {
+        match record.body {
+            Some(body) => {
+                let schedule: Schedule = serde_json::from_str(body.as_ref()).unwrap();
+                let url = format!(
+                    "https://api.test.algorithmia.com/v1/algo/{}",
+                    schedule.algorithm
+                );
+                let res = client
+                    .post(&url)
+                    .header("Content-Type", "text/plain")
+                    .header("Authorization", schedule.apikey)
+                    .body(schedule.input)
+                    .send();
+                match res {
+                    Ok(mut res) => {
+                        let respose_text = res.text().ok().unwrap();
+                        println!("{:?}", respose_text);
+                        send_sqs(result_sqs_url.to_owned(), respose_text)
+                    }
+                    Err(e) => println!("{:?}", e),
+                }
+            }
+            None => (),
+        }
+    }
+
+    Ok(Value::Null)
+}
+
+fn send_sqs(sqs_url: String, message: String) {
+    let client = SqsClient::new(Region::UsEast1);
+    let request = SendMessageRequest {
+        message_body: message,
+        queue_url: sqs_url,
+        ..Default::default()
+    };
+    println!("{:?}", client.send_message(request).sync().ok());
+}
---
+++ lib-models/src/lib.rs
@@ -0,0 +1,133 @@
+#[macro_use]
+extern crate maplit;
+
+extern crate chrono;
+extern crate serde;
+extern crate serde_json;
+extern crate uuid;
+
+use std::env;
+
+use chrono::prelude::*;
+
+use rusoto_core::Region;
+use rusoto_dynamodb::{AttributeValue, DynamoDb, DynamoDbClient, QueryInput, ScanInput};
+use serde::{Deserialize, Serialize};
+use std::collections::hash_map::RandomState;
+use std::collections::HashMap;
+use uuid::Uuid;
+
+#[derive(Deserialize, Serialize, Clone, Debug)]
+pub struct Schedule {
+    pub id: Option<Uuid>,
+    pub enabled: Option<bool>,
+    pub run_interval_minutes: u32,
+    pub apikey: String,
+    pub algorithm: String,
+    pub input: String,
+    pub username: Option<String>,
+}
+
+#[allow(dead_code)]
+impl Schedule {
+    pub fn to_schedule(item: &HashMap<String, AttributeValue, RandomState>) -> Schedule {
+        Schedule {
+            id: item
+                .get("id")
+                .and_then(|x| x.s.as_ref())
+                .map(|x| Uuid::parse_str(&x).expect("error parsing id")),
+            enabled: item.get("enabled").and_then(|x| x.bool.to_owned()),
+            run_interval_minutes: item
+                .get("run_interval_minutes")
+                .and_then(|x| x.n.as_ref())
+                .map(|x| {
+                    x.parse::<u32>()
+                        .expect("error parsing run_interval_minutes")
+                })
+                .expect("missing required run_interval_minutes"),
+            apikey: item
+                .get("apikey")
+                .and_then(|x| x.s.to_owned())
+                .expect("missing required apikey"),
+            algorithm: item
+                .get("algorithm")
+                .and_then(|x| x.s.to_owned())
+                .expect("missing requred algorithm"),
+            input: item
+                .get("input")
+                .and_then(|x| x.s.to_owned())
+                .expect("missing required input"),
+            username: item.get("username").and_then(|x| x.s.to_owned()),
+        }
+    }
+
+    pub fn from_schedule(schedule: Schedule) -> HashMap<String, AttributeValue> {
+        hashmap! {
+            String::from("username") =>  AttributeValue { s: schedule.username, ..Default::default() },
+            String::from("id") => AttributeValue { s: schedule.id.map(|uid| uid.to_string()), ..Default::default() },
+            String::from("enabled") =>  AttributeValue { bool: schedule.enabled, ..Default::default() },
+            String::from("run_interval_minutes") =>  AttributeValue { n: Some(schedule.run_interval_minutes.to_string()), ..Default::default() },
+            String::from("apikey") =>  AttributeValue { s: Some(schedule.apikey), ..Default::default() },
+            String::from("algorithm") =>  AttributeValue { s: Some(schedule.algorithm), ..Default::default() },
+            String::from("input") =>  AttributeValue { s: Some(schedule.input), ..Default::default() },
+        }
+    }
+
+    pub fn get_enabled_schedules() -> Vec<Schedule> {
+        let attribute_map = hashmap! {
+            String::from(":enabled") => AttributeValue { bool: Some(true), ..Default::default() }
+        };
+
+        let scan_input = ScanInput {
+            consistent_read: Some(true),
+            filter_expression: Some(String::from("enabled = :enabled")),
+            expression_attribute_values: Some(attribute_map),
+            table_name: env::var("USER_SCHEDULE_TABLE_NAME")
+                .expect("env variable USER_SCHEDULE_TABLE_NAME not found"),
+            ..Default::default()
+        };
+
+        let client = DynamoDbClient::new(Region::UsEast1);
+        let minutes = Local::now().minute();
+        match client.scan(scan_input).sync() {
+            Ok(output) => match output.items {
+                None => vec![],
+                Some(items) => items
+                    .iter()
+                    .map(Schedule::to_schedule)
+                    .filter(|x| minutes % x.run_interval_minutes == 0)
+                    .collect(),
+            },
+            Err(error) => {
+                println!("{}", error.to_string());
+                vec![]
+            }
+        }
+    }
+
+    pub fn get_schedules_by_username(username: String) -> Vec<Schedule> {
+        let attribute_map = hashmap! {
+            String::from(":username") => AttributeValue { s: Some(username), ..Default::default() }
+        };
+
+        let query_input = QueryInput {
+            consistent_read: Some(true),
+            key_condition_expression: Some(String::from("username = :username")),
+            expression_attribute_values: Some(attribute_map),
+            table_name: env::var("USER_SCHEDULE_TABLE_NAME").expect("env variable not found"),
+            ..Default::default()
+        };
+
+        let client = DynamoDbClient::new(Region::UsEast1);
+        match client.query(query_input).sync() {
+            Ok(output) => match output.items {
+                None => vec![],
+                Some(items) => items.iter().map(Schedule::to_schedule).collect(),
+            },
+            Err(error) => {
+                println!("{}", error.to_string());
+                vec![]
+            }
+        }
+    }
+}
